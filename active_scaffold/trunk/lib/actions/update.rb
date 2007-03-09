57e2668586dade86fc0a5b5a9738e0c051a61215
rrwhite <rrwhite@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-03-09 04:31:52 +0000

    made a copy

---
+++ active_scaffold/trunk/lib/actions/update.rb
@@ -0,0 +1,69 @@
+module ActiveScaffold::Actions
+  module Update
+    include Base
+
+    def self.included(base)
+      super
+      base.verify :method => [:post, :put],
+                  :only => :update,
+                  :redirect_to => { :action => :index }
+    end
+
+    def edit
+      insulate { do_edit }
+
+      respond_to do |type|
+        type.html do
+          if successful?
+            render(:action => 'update_form', :layout => true)
+          else
+            return_to_main
+          end
+        end
+        type.js do
+          render(:partial => 'update_form', :layout => false)
+        end
+      end
+    end
+
+    def update
+      insulate { do_update }
+
+      respond_to do |type|
+        type.html do
+          if successful?
+            flash[:info] = _('UPDATED %s', @record.to_label)
+            return_to_main
+          else
+            render(:action => 'update_form', :layout => true)
+          end
+        end
+        type.js do
+          if successful?
+            render :action => 'update.rjs', :layout => false
+          else
+            render :action => 'form_messages.rjs', :layout => false
+          end
+        end
+        type.xml { render :xml => response_object.to_xml, :content_type => Mime::XML, :status => response_status }
+        type.json { render :text => response_object.to_json, :content_type => Mime::JSON, :status => response_status }
+        type.yaml { render :text => response_object.to_yaml, :content_type => Mime::YAML, :status => response_status }
+      end
+    end
+
+    protected
+
+    def do_edit
+      @record = find_if_allowed(params[:id], 'update')
+    end
+
+    def do_update
+      @record = find_if_allowed(params[:id], 'update')
+      active_scaffold_config.model.transaction do
+        @record = update_record_from_params(@record, active_scaffold_config.update.columns, params[:record])
+        # TODO: make this a "recursive" save
+        @record.save
+      end
+    end
+  end
+end

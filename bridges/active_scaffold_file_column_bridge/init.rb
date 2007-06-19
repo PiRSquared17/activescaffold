3cee9602e57f441e42de701f8d7a88b8e16fdd53
timcharper <timcharper@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-06-19 21:50:25 +0000

    workaround: merged all into one file (having files named the same as files from active scaffold was causing strange "file not found" errors)

--- bridges/active_scaffold_file_column_bridge/init.rb
+++ bridges/active_scaffold_file_column_bridge/init.rb
@@ -1,7 +1,1 @@
-[
-  "config/core.rb", 
-  "helpers/form_column_helpers.rb", 
-  "helpers/list_column_helpers.rb"
-].each { |file|
-  load File.join( File.dirname(__FILE__), "lib",file)
-}
+require "active_scaffold_file_column_bridge"
--- bridges/active_scaffold_file_column_bridge/lib/active_scaffold_file_column_bridge.rb
+++ bridges/active_scaffold_file_column_bridge/lib/active_scaffold_file_column_bridge.rb
@@ -27,3 +27,34 @@ module ActiveScaffold::Config
     
   end
 end
+
+module ActiveScaffold
+  module Helpers
+    # Helpers that assist with the rendering of a List Column
+    module ListColumns
+      def render_column_with_file_column(record, column)
+        if column_override?(column) || !active_scaffold_config.file_column_fields.include?(column.name.to_sym)
+          return render_column_without_file_column(record, column)
+        end
+        
+        value = record.send(column.name)
+        return "" if value.nil?
+        link_to File.basename(value), url_for_file_column(record, column.name.to_s), :popup => true 
+      end
+      
+      alias_method_chain :render_column, :file_column
+    end
+  end
+end
+
+
+module ActiveScaffold
+  module Helpers
+    # Helpers that assist with the rendering of a Form Column
+    module FormColumns
+      def active_scaffold_input_file_column(column, options)
+        file_column_field("record", column.name, options)
+      end      
+    end
+  end
+end

d57c6052fdc5c7177c4489a8fc1040423ddd7064
timcharper <timcharper@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-06-28 15:39:00 +0000

    updated to work with latest trunk active_scaffold
    render_column -> render_list_column

--- bridges/active_scaffold_file_column_bridge/lib/active_scaffold_file_column_bridge.rb
+++ bridges/active_scaffold_file_column_bridge/lib/active_scaffold_file_column_bridge.rb
@@ -41,9 +41,9 @@ module ActiveScaffold
   module Helpers
     # Helpers that assist with the rendering of a List Column
     module ListColumns
-      def render_column_with_file_column(record, column)
+      def render_list_column_with_file_column(record, column)
         if column_override?(column) || !active_scaffold_config.file_column_fields.include?(column.name.to_sym)
-          return render_column_without_file_column(record, column)
+          return render_list_column_without_file_column(record, column)
         end
         
         value = record.send(column.name)
@@ -66,7 +66,7 @@ module ActiveScaffold
           :popup => true)
       end
       
-      alias_method_chain :render_column, :file_column
+      alias_method_chain :render_list_column, :file_column
     end
   end
 end

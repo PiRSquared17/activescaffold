5cb1fed8a2ab4cc73f5d943ca2b84296eab857a6
timcharper <timcharper@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-06-28 16:03:09 +0000

    actually, it's get_column_value

--- bridges/active_scaffold_file_column_bridge/lib/active_scaffold_file_column_bridge.rb
+++ bridges/active_scaffold_file_column_bridge/lib/active_scaffold_file_column_bridge.rb
@@ -41,9 +41,9 @@ module ActiveScaffold
   module Helpers
     # Helpers that assist with the rendering of a List Column
     module ListColumns
-      def render_list_column_with_file_column(record, column)
+      def get_column_value_with_file_column(record, column)
         if column_override?(column) || !active_scaffold_config.file_column_fields.include?(column.name.to_sym)
-          return render_list_column_without_file_column(record, column)
+          return get_column_value_without_file_column(record, column)
         end
         
         value = record.send(column.name)
@@ -59,14 +59,14 @@ module ActiveScaffold
           end
         end
                 
-        return "" if value.nil?
+        return "&nbsp;" if value.nil?
         
         link_to( (column.file_column_display==:link) ? File.basename(value) : image_tag(url_for_file_column(record, column.name.to_s, "thumb"), :border => 0), 
           url_for_file_column(record, column.name.to_s), 
           :popup => true)
       end
       
-      alias_method_chain :render_list_column, :file_column
+      alias_method_chain :get_column_value, :file_column
     end
   end
 end
